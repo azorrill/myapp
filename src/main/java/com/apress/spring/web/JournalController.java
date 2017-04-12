d13fe32b40b222e055c9ef799cef0422ffe7e1a8
azorrill <alex.zorrilla@emc.com>
2017-04-12 18:56:23 -0400

    Add /journal controller returning a json object

---
+++ src/main/java/com/apress/spring/web/JournalController.java
@@ -0,0 +1,36 @@
+package com.apress.spring.web;
+
+import java.util.List;
+
+import com.apress.spring.domain.Journal;
+import com.apress.spring.repository.JournalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.awt.*;
+
+/**
+ * Created by azorrill on 4/12/17.
+ */
+@Controller
+public class JournalController {
+
+    @Autowired
+    JournalRepository repo;
+
+    @RequestMapping(value="/journal", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public @ResponseBody List<Journal> getJournal(){
+        return repo.findAll();
+    }
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("journal", repo.findAll());
+        return "index";
+    }
+
+}
